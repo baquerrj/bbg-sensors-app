0f7549cc2b6707ebe5d055518967a0abf21d1ab3
Roberto Baquerizo <roba8460@colorado.edu>
2019-03-31 22:11:12 -0600

    implemented watchdog requesting alive response from temperature task thread

--- src/watchdog.c
+++ src/watchdog.c
@@ -21,27 +21,45 @@
 
 #include "watchdog.h"
 #include "common.h"
-
+#include "temperature.h"
 #include <signal.h>
 #include <errno.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
+#include <fcntl.h>
+#include <mqueue.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #define NUM_THREADS  2
-/* Threads to ping */
-//static pthread_t threads[NUM_THREADS];
-static struct thread_id_s* threads;
 
+static timer_t    timerid;
+struct itimerspec trigger;
+
+static struct thread_id_s* threads;
+static mqd_t thread_msg_q[NUM_THREADS];
+
+/*
+ * =================================================================================
+ * Function:       sig_handler
+ * @brief
+ *
+ * @param  <+NAME+> <+DESCRIPTION+>
+ * @return <+DESCRIPTION+>
+ * <+DETAILED+>
+ * =================================================================================
+ */
 static void sig_handler( int signo )
 {
    if( SIGUSR2 == signo )
    {
-      fprintf( stdout, "watchdog caught signals - killing thread [%ld]\n", threads->t1 );
+      fprintf( stdout, "watchdog caught signals - killing thread [%ld]\n", threads->temp_thread );
       fflush( stdout );
-      pthread_kill( threads->t1, SIGUSR1 );
-      fprintf( stdout, "watchdog caught signals - killing thread [%ld]\n", threads->t2 );
+      pthread_kill( threads->temp_thread, SIGUSR1 );
+      fprintf( stdout, "watchdog caught signals - killing thread [%ld]\n", threads->logger_thread );
       fflush( stdout );
-      pthread_kill( threads->t2, SIGUSR1 );
+      pthread_kill( threads->logger_thread, SIGUSR1 );
       free( threads );
       thread_exit( 0 );
    }
@@ -57,10 +75,71 @@ static void sig_handler( int signo )
  * <+DETAILED+>
  * =================================================================================
  */
-int check_threads( void )
+void check_threads( union sigval sig )
 {
+   int retVal = 0;
+   request_t request = {0};
+   request.id = REQUEST_STATUS;
+   retVal = mq_send( thread_msg_q[0], (const char*)&request, sizeof( request ), 0 );
+
+   if( 0 > retVal )
+   {
+      int errnum = errno;
+      fprintf( stderr, "Encountered error sending status request from watchdog: (%s)\n",
+               strerror( errnum ) );
+   }
+   return;
+}
+
+/*
+ * =================================================================================
+ * Function:       setup_timer
+ * @brief
+ *
+ * @param  <+NAME+> <+DESCRIPTION+>
+ * @return <+DESCRIPTION+>
+ * <+DETAILED+>
+ * =================================================================================
+ */
+int setup_timer( void )
+{
+   int retVal = 0;
+   /* Set up timer */
+   char info[] = "timer woken up!";
+   struct sigevent sev;
+
+   memset(&sev, 0, sizeof(struct sigevent));
+   memset(&trigger, 0, sizeof(struct itimerspec));
+
+   sev.sigev_notify = SIGEV_THREAD;
+   sev.sigev_notify_function = &check_threads;
+   sev.sigev_value.sival_ptr = &info;
+   sev.sigev_notify_attributes = NULL;
+
+   retVal = timer_create(CLOCK_REALTIME, &sev, &timerid);
+   if( 0 > retVal )
+   {
+      int errnum = errno;
+      fprintf( stderr, "Encountered error creating new timer for watchdog thread: (%s)\n",
+               strerror( errnum ) );
+      return retVal;
+   }
+
+   trigger.it_value.tv_sec = 1;
+   trigger.it_interval.tv_nsec = 100 * 1000000;
+//   trigger.it_interval.tv_sec = 1;
+
+   retVal = timer_settime( timerid, 0, &trigger, NULL );
+   if( 0 > retVal )
+   {
+      int errnum = errno;
+      fprintf( stderr, "Encountered error creating new timer for watchdog thread: (%s)\n",
+               strerror( errnum ) );
+      return retVal;
+   }
    return 0;
 }
+
 
 /*
  * =================================================================================
@@ -72,8 +151,13 @@ int check_threads( void )
  * <+DETAILED+>
  * =================================================================================
  */
-int watchdog_init( pthread_t *threads )
+int watchdog_init( void )
 {
+   while( 0 == (thread_msg_q[0] = get_temperature_queue()) );
+
+   fprintf( stderr, "Watchdog says: Temp Queue FD: %d\n", thread_msg_q[0] );
+   setup_timer();
+
    return 0;
 }
 
@@ -101,13 +185,11 @@ void *watchdog_fn( void *thread_args )
    {
       threads = malloc( sizeof( struct thread_id_s ) );
       threads = (struct thread_id_s*)thread_args;
-
-//      fprintf( stdout,"Processed thread %ld\n", threads->t1 );
-//      fprintf( stdout,"Processed thread %ld\n", threads->t2 );
    }
+
+   watchdog_init();
+
    while(1);
-
-
    return NULL;
 }
 
