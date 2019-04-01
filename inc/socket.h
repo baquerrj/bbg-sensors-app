1bab54f68e6a2d26cfb9b0e694133fca45e9bb5c
Roberto Baquerizo <roba8460@colorado.edu>
2019-03-31 19:23:02 -0600

    socket server implementation

---
+++ inc/socket.h
@@ -0,0 +1,66 @@
+/*
+ * =================================================================================
+ *    @file     socket.h
+ *    @brief    
+ *
+ *  <+DETAILED+>
+ *
+ *    @author   Roberto Baquerizo (baquerrj), roba8460@colorado.edu
+ *
+ *    @internal
+ *       Created:  03/31/2019
+ *      Revision:  none
+ *      Compiler:  gcc
+ *  Organization:  University of Colorado: Boulder
+ *
+ *  This source code is released for free distribution under the terms of the
+ *  GNU General Public License as published by the Free Software Foundation.
+ * =================================================================================
+ */
+
+
+#ifndef  SOCKET_H
+#define  SOCKET_H
+
+
+#include "common.h"
+
+/*
+ * =================================================================================
+ * Function:       process_request
+ * @brief  
+ *
+ * @param  <+NAME+> <+DESCRIPTION+>
+ * @return <+DESCRIPTION+>
+ * <+DETAILED+>
+ * =================================================================================
+ */
+response_t process_request( request_t *request );
+
+/*
+ * =================================================================================
+ * Function:       socket_init
+ * @brief  
+ *
+ * @param  <+NAME+> <+DESCRIPTION+>
+ * @return <+DESCRIPTION+>
+ * <+DETAILED+>
+ * =================================================================================
+ */
+int socket_init( void );
+
+
+/*
+ * =================================================================================
+ * Function:       socket_call_back
+ * @brief  
+ *
+ * @param  <+NAME+> <+DESCRIPTION+>
+ * @return <+DESCRIPTION+>
+ * <+DETAILED+>
+ * =================================================================================
+ */
+void *socket_fn( void *thread_arg );
+
+
+#endif   /* SOCKET_H */
