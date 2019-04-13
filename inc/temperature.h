81e531a56b81f5a770de6de013b155afd9db4a2b
Roberto Baquerizo <roba8460@colorado.edu>
2019-04-12 20:27:47 -0600

    split up implementation of tmp102 sensor task same as apds9301

--- inc/temperature.h
+++ inc/temperature.h
@@ -1,5 +1,4 @@
-/**
- * =================================================================================
+/*!
  *    @file     temperature.h
  *    @brief   Header for temperature sensor thread 
  *
@@ -14,171 +13,75 @@
  *
  *  This source code is released for free distribution under the terms of the
  *  GNU General Public License as published by the Free Software Foundation.
- * =================================================================================
  */
 
 #ifndef TEMPERATURE_H
 #define TEMPERATURE_H
 
 #include "common.h"
-#include "i2c.h"
 
 #include <mqueue.h>
 
 #define TEMP_QUEUE_NAME "/temperature-queue"
 
-/** Default address for Temperature Sensor TMP102 */
-#define TMP102_SLAVE       (0x48)
+/*!
+ * Function:       tmp102_handler
+ * @brief   Timer handler function for temperature sensor thread
+ *          When woken up by the timer, get temperature and write state to shared memory
+ *
+ * @param   sig
+ * @return  void
+ */
+void tmp102_handler( union sigval sig );
 
-/** Regsiter addresses for TMP102 */
-#define TMP102_REG_TEMP    (0x00)
-#define TMP102_REG_CONFIG  (0x01)
-#define TMP102_TLOW        (0x02)
-#define TMP102_THIGH       (0x03)
-
-/** Default configuration */
-#define TMP102_SHUTDOWN_MODE     (1)
-#define TMP102_THERMOSTAT_MODE   (1)
-#define TMP102_POLARITY          (1)
-#define TMP102_FAULT_QUEUE       (1)
-#define TMP102_RESOLUTION_0      (2)
-#define TMP102_RESOLUTION_1      (4)
-#define TMP102_EXTENDED_MODE     (0)
-#define TMP102_CONVERSION_RATE   (2)
+/*!
+ * Function:       tmp102_cycle
+ * @brief   Cycle function for temperature sensor thread
+ *          We wait in this while loop checking for requests from watchdog for health status
+ *
+ * @param   void
+ * @return  void 
+ */
+void tmp102_cycle( void );
 
 
-typedef struct {
-   uint16_t res_0;
-   uint16_t res_1;
-} conv_res_t;
-
-typedef struct {
-   uint16_t shutdown;
-   uint16_t thermostat;
-} tmp102_mode_t;
-
-typedef struct {
-   tmp102_mode_t mode;     /* shutdown and thermostat modes */
-   uint16_t polarity;      /* polarity */
-   uint16_t fault_queue;   /* fault queue */
-   conv_res_t resolution;  /* converter resolution */
-   uint16_t one_shot;      /* when in shutdown mode, writing 1 starts a single conversion */
-   uint16_t operation;     /* extended vs normal operation */
-   uint16_t alert;         /* alerrt bit */
-   uint16_t conv_rate;     /* conversion rate */
-} tmp102_config_t;
-
-/**
- * =================================================================================
+/*!
  * Function:       get_temperature
  * @brief   Returns last temperature reading we have
  *
  * @param   void
  * @return  last_temp_value - last temperature reading we have
  * <+DETAILED+>
- * =================================================================================
  */
 float get_temperature( void );
 
-/**
- * =================================================================================
- * Function:       tmp102_write_config
- * @brief   Write configuration register of TMP102 sensor
- *
- * @param   *config_reg  - pointer to struct with values to write to configuration register
- * @return  see i2c_write()
- * =================================================================================
- */
-int tmp102_write_config( tmp102_config_t *config_reg );
 
-/**
- * =================================================================================
- * Function:       tmp102_get_temp
- * @brief   Read temperature registers fo TMP102 sensor and decode temperature value
- *
- * @param   *temperature - pointer to location to write decoded value to
- * @return  EXIT_CLEAN if successful, otherwise EXIT_ERROR
- * <+DETAILED+>
- * =================================================================================
- */
-int tmp102_get_temp( float *temperature );
-
-/**
- * =================================================================================
- * Function:       tmp102_write_thigh
- * @brief   Write value thigh (in celsius) to Thigh register for TMP102 sensor
- *
- * @param   thigh - value to write to Thigh register
- * @return  EXIT_CLEAN if successful, otherwise EXIT_ERROR
- * =================================================================================
- */
-int tmp102_write_thigh( float thigh );
-
-/**
- * =================================================================================
- * Function:       tmp102_write_tlow
- * @brief   Write value tlow (in celsius) to Tlow register for TMP102 sensor
- *
- * @param   tlow - value to write to Tlow register
- * @return  EXIT_CLEAN if successful, otherwise EXIT_ERROR
- * =================================================================================
- */
-int tmp102_write_tlow( float tlow );
-
-/**
- * =================================================================================
- * Function:       tmp102_read_thigh
- * @brief   Read value of THigh register of TMP102 sensor and store value (in celsius) in thigh
- *
- * @param   thigh - pointer to location to store decoded temperature value to
- * @return  EXIT_CLEAN if successful, EXIT_ERROR otherwise
- * =================================================================================
- */
-int tmp102_read_thigh( float *thigh );
-
-/**
- * =================================================================================
- * Function:       tmp102_read_tlow
- * @brief   Read value of TLow register of TMP102 sensor and store value (in celsius) in tlow
- *
- * @param   tlow  - pointer to location to store decoded temperature value to
- * @return  EXIT_CLEAN if successful, EXIT_ERROR otherwise
- * =================================================================================
- */
-int tmp102_read_tlow( float *tlow );
-
-/**
- * =================================================================================
+/*!
  * Function:       get_temperature_queue
  * @brief   Get file descriptor for temperature sensor thread.
  *          Called by watchdog thread in order to be able to send heartbeat check via queue
  *
  * @param   void
  * @return  temp_queue - file descriptor for temperature sensor thread message queue
- * =================================================================================
  */
 mqd_t get_temperature_queue( void );
 
-/**
- * =================================================================================
+/*!
  * Function:       temp_queue_init
  * @brief   Initialize message queue for temperature sensor thread
  *
  * @param   void
  * @return  msg_q - file descriptor for initialized message queue
- * =================================================================================
  */
 int temp_queue_init( void );
 
-/**
- * =================================================================================
+/*!
  * Function:       temperature_fn
  * @brief   Entry point for temperature sensor processing thread
  *
  * @param   thread_args  - void ptr to arguments used to initialize thread
  * @return  NULL  - We don't really exit from this function, 
  *                   since the exit point is thread_exit()
- * =================================================================================
  */
 void *temperature_fn( void *thread_args );
 
--- src/temperature.c
+++ src/temperature.c
@@ -1,5 +1,4 @@
-/**
- * =================================================================================
+/*!
  *    @file     temperature.c
  *    @brief   Source file implementing temperature.h
  *
@@ -14,9 +13,9 @@
  *
  *  This source code is released for free distribution under the terms of the
  *  GNU General Public License as published by the Free Software Foundation.
- * =================================================================================
  */
 
+#include "tmp102_sensor.h"
 #include "watchdog.h"
 #include "temperature.h"
 #include "led.h"
@@ -33,258 +32,12 @@ static float last_temp_value = -5;
 static mqd_t temp_queue;
 static shared_data_t *shm;
 
-const tmp102_config_t tmp102_default_config = {
-   .mode = {                                 /* shutdown and thermostat modes */
-      .shutdown = TMP102_SHUTDOWN_MODE,
-      .thermostat = TMP102_THERMOSTAT_MODE
-   },
-   .polarity = TMP102_POLARITY,              /* polarity */
-   .fault_queue = TMP102_FAULT_QUEUE,        /* fault queue */
-   .resolution = {                           /* converter resolution */
-      .res_0 = TMP102_RESOLUTION_0,
-      .res_1 = TMP102_RESOLUTION_1
-   },
-   .one_shot = 1,                            /* when in shutdown mode, writing 1 starts a single conversion */
-   .operation = TMP102_EXTENDED_MODE,        /* extended vs normal operation */
-   .alert = 1,                               /* alter bit */
-   .conv_rate = TMP102_CONVERSION_RATE       /* conversion rate */
-};
-
-
-
-/**
- * =================================================================================
- * Function:       get_temperature
- * @brief   Returns last temperature reading we have
- *
- * @param   void
- * @return  last_temp_value - last temperature reading we have
- * <+DETAILED+>
- * =================================================================================
- */
 float get_temperature( void )
 {
    return last_temp_value;
 }
 
-
-/**
- * =================================================================================
- * Function:       tmp102_write_config
- * @brief   Write configuration register of TMP102 sensor
- *
- * @param   *config_reg  - pointer to struct with values to write to configuration register
- * @return  see i2c_write()
- * =================================================================================
- */
-int tmp102_write_config( tmp102_config_t *config_reg )
-{
-   int retVal = i2c_write( TMP102_SLAVE, TMP102_REG_CONFIG, *((uint16_t*)&config_reg) );
-
-   return retVal;
-}
-
-/**
- * =================================================================================
- * Function:       tmp102_get_temp
- * @brief   Read temperature registers fo TMP102 sensor and decode temperature value
- *
- * @param   *temperature - pointer to location to write decoded value to
- * @return  EXIT_CLEAN if successful, otherwise EXIT_ERROR
- * =================================================================================
- */
-int tmp102_get_temp( float *temperature )
-{
-   uint8_t buffer[2] = {0};
-   int retVal = i2c_read( TMP102_SLAVE, TMP102_REG_TEMP, buffer, sizeof( buffer ) );
-   if( 0 > retVal )
-   {
-      return EXIT_ERROR;
-   }
-
-   uint16_t tmp = 0;
-   tmp = 0xfff & ( ((uint16_t)buffer[0] << 4 ) | (buffer[1] >> 4 ) ); /* buffer[0] = MSB(15:8)
-                                                                        buffer[1] = LSB(7:4) */
-   if( 0x800 & tmp )
-   {
-      tmp = ( (~tmp ) + 1 ) & 0xfff;
-      *temperature = -1.0 * (float)tmp * 0.0625;
-   }
-   else
-   {
-      *temperature = ((float)tmp) * 0.0625;
-   }
-
-   return EXIT_CLEAN;
-}
-
-/**
- * =================================================================================
- * Function:       tmp102_write_thigh
- * @brief   Write value thigh (in celsius) to Thigh register for TMP102 sensor
- *
- * @param   thigh - value to write to Thigh register
- * @return  EXIT_CLEAN if successful, otherwise EXIT_ERROR
- * =================================================================================
- */
-int tmp102_write_thigh( float thigh )
-{
-   if( (-56.0 > thigh) || (151.0 < thigh) )
-   {
-      thigh = 80.0;
-   }
-
-   thigh /= 0.0625;
-   uint16_t tmp;
-
-   if( 0 > thigh )
-   {
-      tmp = ( (uint16_t)thigh << 4 );
-      tmp &= 0x7fff;
-   }
-   else
-   {
-      thigh *= -1;
-      tmp = (uint16_t)thigh;
-      tmp = ~(tmp) + 1;
-      tmp = tmp << 4;
-   }
-
-   int retVal = i2c_write( TMP102_SLAVE, TMP102_THIGH, tmp );
-   if( 0 > retVal )
-   {
-      sem_wait(&shm->w_sem);
-      print_header(shm->header);
-      sprintf( shm->buffer, "Could not write value to THigh register!\n" );
-      sem_post(&shm->r_sem);
-      return EXIT_ERROR;
-   }
-
-   return EXIT_CLEAN;
-}
-
-/**
- * =================================================================================
- * Function:       tmp102_write_tlow
- * @brief   Write value tlow (in celsius) to Tlow register for TMP102 sensor
- *
- * @param   tlow - value to write to Tlow register
- * @return  EXIT_CLEAN if successful, otherwise EXIT_ERROR
- * =================================================================================
- */
-int tmp102_write_tlow( float tlow )
-{
-   if( (-56.0 > tlow) || (151.0 < tlow ) )
-   {
-      tlow = 75.0;
-   }
-
-   tlow /= 0.0625;
-   uint16_t tmp;
-
-   if( 0 < tlow )
-   {
-      tmp = ( (uint16_t)tlow << 4 );
-      tmp &= 0x7fff;
-   }
-   else
-   {
-      tlow *= -1;
-      tmp = (uint16_t)tlow;
-      tmp = ~(tmp) + 1;
-      tmp = tmp << 4;
-   }
-
-   int retVal = i2c_write( TMP102_SLAVE, TMP102_TLOW, tmp );
-   if( 0 > retVal )
-   {
-      sem_wait(&shm->w_sem);
-      print_header(shm->header);
-      sprintf( shm->buffer, "Could not write value to TLow register!\n" );
-      sem_post(&shm->r_sem);
-      return EXIT_ERROR;
-   }
-
-   return EXIT_CLEAN;
-}
-
-/**
- * =================================================================================
- * Function:       tmp102_read_thigh
- * @brief   Read value of THigh register of TMP102 sensor and store value (in celsius) in thigh
- *
- * @param   thigh - pointer to location to store decoded temperature value to
- * @return  EXIT_CLEAN if successful, EXIT_ERROR otherwise
- * =================================================================================
- */
-int tmp102_read_thigh( float *thigh )
-{
-   uint16_t tmp = 0;
-
-   int retVal = i2c_read( TMP102_SLAVE, TMP102_THIGH, (uint8_t*)&tmp, sizeof( tmp ) );
-   if( 0 > retVal )
-   {
-      sem_wait(&shm->w_sem);
-      print_header(shm->header);
-      sprintf( shm->buffer, "Could not read from TLow register!\n" );
-      sem_post(&shm->r_sem);
-      return EXIT_ERROR;
-   }
-
-   if( tmp & 0x800 )
-   {
-      tmp = ~(tmp) + 1;
-      *thigh = -1 * ( (float)tmp * 0.0625 );
-   }
-   else
-   {
-      *thigh = (float)tmp * 0.0625;
-   }
-
-   return EXIT_CLEAN;
-}
-
-
-/**
- * =================================================================================
- * Function:       tmp102_read_tlow
- * @brief   Read value of TLow register of TMP102 sensor and store value (in celsius) in tlow
- *
- * @param   tlow  - pointer to location to store decoded temperature value to
- * @return  EXIT_CLEAN if successful, EXIT_ERROR otherwise
- * =================================================================================
- */
-int tmp102_read_tlow( float *tlow )
-{
-   uint16_t tmp = 0;
-
-   int retVal = i2c_read( TMP102_SLAVE, TMP102_TLOW, (uint8_t*)&tmp, sizeof( tmp ) );
-   if( 0 > retVal )
-   {
-      sem_wait(&shm->w_sem);
-      print_header(shm->header);
-      sprintf( shm->buffer, "Could not read from TLow register!\n" );
-      sem_post(&shm->r_sem);
-      return retVal;
-   }
-
-   if( tmp & 0x800 )
-   {
-      tmp = ~(tmp) + 1;
-      *tlow = -1 * (float)tmp * 0.0625;
-   }
-   else
-   {
-      *tlow = (float)tmp * 0.0625;
-   }
-
-   return retVal;
-}
-
-
-
-/**
- * =================================================================================
+/*!
  * Function:       sig_handler
  * @brief   Signal handler for temperature sensor thread.
  *          On normal operation, we should be receving SIGUSR1/2 signals from watchdog
@@ -292,7 +45,6 @@ int tmp102_read_tlow( float *tlow )
  *
  * @param   signo - enum with signal number of signal being handled
  * @return  void
- * =================================================================================
  */
 static void sig_handler( int signo )
 {
@@ -315,17 +67,7 @@ static void sig_handler( int signo )
    return;
 }
 
-/**
- * =================================================================================
- * Function:       timer_handler
- * @brief   Timer handler function for temperature sensor thread
- *          When woken up by the timer, get temperature and write state to shared memory
- *
- * @param   sig
- * @return  void
- * =================================================================================
- */
-static void timer_handler( union sigval sig )
+void tmp102_handler( union sigval sig )
 {
    static int i = 0;
    led_toggle( LED0_BRIGHTNESS );
@@ -337,11 +79,11 @@ static void timer_handler( union sigval sig )
    i++;
    if( retVal )
    {
-      sprintf( shm->buffer, "cycle[%d]: %0.5f Celsius\n", i, temperature );
+      sprintf( shm->buffer, "cycle[%d]: could not get temperature reading!\n", i );
    }
    else
    {
-      sprintf( shm->buffer, "cycle[%d]: could not get temperature reading!\n", i );
+      sprintf( shm->buffer, "cycle[%d]: %0.5f Celsius\n", i, temperature );
    }
 
    sem_post(&shm->r_sem);
@@ -350,17 +92,7 @@ static void timer_handler( union sigval sig )
 }
 
 
-/**
- * =================================================================================
- * Function:       cycle
- * @brief   Cycle function for temperature sensor thread
- *          We wait in this while loop checking for requests from watchdog for health status
- *
- * @param   void
- * @return  void 
- * =================================================================================
- */
-static void cycle( void )
+void tmp102_cycle( void )
 {
    int retVal = 0;
    msg_t request = {0};
@@ -401,30 +133,11 @@ static void cycle( void )
    return;
 }
 
-/**
- * =================================================================================
- * Function:       get_temperature_queue
- * @brief   Get file descriptor for temperature sensor thread.
- *          Called by watchdog thread in order to be able to send heartbeat check via queue
- *
- * @param   void
- * @return  temp_queue - file descriptor for temperature sensor thread message queue
- * =================================================================================
- */
 mqd_t get_temperature_queue( void )
 {
    return temp_queue;
 }
 
-/**
- * =================================================================================
- * Function:       temp_queue_init
- * @brief   Initialize message queue for temperature sensor thread
- *
- * @param   void
- * @return  msg_q - file descriptor for initialized message queue
- * =================================================================================
- */
 int temp_queue_init( void )
 {
    /* unlink first in case we hadn't shut down cleanly last time */
@@ -449,16 +162,6 @@ int temp_queue_init( void )
    return msg_q;
 }
 
-/**
- * =================================================================================
- * Function:       temperature_fn
- * @brief   Entry point for temperature sensor processing thread
- *
- * @param   thread_args - void ptr to arguments used to initialize thread
- * @return  NULL  - We don't really exit from this function, 
- *                   since the exit point is thread_exit()
- * =================================================================================
- */
 void *temperature_fn( void *thread_args )
 {
    /* Get time that thread was spawned */
@@ -493,10 +196,10 @@ void *temperature_fn( void *thread_args )
       thread_exit( EXIT_INIT );
    }
 
-   timer_setup( &timerid, &timer_handler );
+   timer_setup( &timerid, &tmp102_handler );
 
-   timer_start( &timerid, 1000000 );
-   cycle();
+   timer_start( &timerid, FREQ_1HZ );
+   tmp102_cycle();
 
    thread_exit( 0 );
    return NULL;
